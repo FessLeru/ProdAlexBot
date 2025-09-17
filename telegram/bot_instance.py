b362c8fda422c2d8400e8ecdd5482af982d164d2
FessLeru <iisusputmoj@gmail.com>
2025-09-17 20:55:16 +0300

    fix bot & add test file

--- telegram/bot_instance.py
+++ telegram/bot_instance.py
@@ -1,7 +1,9 @@
 """Экземпляр Telegram бота."""
 from telebot.async_telebot import AsyncTeleBot
+from telebot import asyncio_filters
 
 from config.settings import settings
 from telegram.states import state_storage
 
 bot = AsyncTeleBot(settings.BOT_TOKEN, state_storage=state_storage)
+bot.add_custom_filter(asyncio_filters.StateFilter(bot))
